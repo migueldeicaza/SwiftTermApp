415b543ffd776517448d6f0960298b597bc5737e
Miguel de Icaza <miguel@gnome.org>
2020-05-05 09:18:37 -0400

    Add LICENSE

---
+++ SwiftTermApp/Keys/AddKeyFromFile.swift
@@ -0,0 +1,21 @@
+//
+//  AddKeyFromFile.swift
+//  SwiftTermApp
+//
+//  Created by Miguel de Icaza on 5/4/20.
+//  Copyright © 2020 Miguel de Icaza. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddKeyFromFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddKeyFromFile_Previews: PreviewProvider {
+    static var previews: some View {
+        AddKeyFromFile()
+    }
+}
