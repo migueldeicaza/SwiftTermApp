eb6a9bd2db130932386e5b80cbe26ad7fd13632b
Miguel de Icaza <miguel@gnome.org>
2021-05-08 20:31:16 -0400

    Small touch up

--- SwiftTermApp/Terminal/TerminalViewController.swift
+++ SwiftTermApp/Terminal/TerminalViewController.swift
@@ -71,7 +71,7 @@ class TerminalViewController: UIViewController {
             } else {
                 tv.applyTheme(theme: settings.getTheme(themeName: host.style))
             }
-            tv.feed(text: "Welcome to SwiftTerm\n\n")
+            tv.feed(text: "Welcome to SwiftTerm\r\n\n")
             return tv
         } catch MyError.noValidKey(let msg) {
             terminalViewCreationError (msg)
